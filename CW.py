6cdeca56fb8f1bcc31681e968347cffcb3da527b
rdunavant0 <rdunavant0@gmail.com>
2019-01-25 11:53:38 -0600

    message

---
+++ CW.py
@@ -0,0 +1,23 @@
+def problem1():
+    userInput = ""
+    while(userInput != 'q'):
+        userInput = input("Enter numbers")
+        if userInput not = 'q':
+            check(must be valid number)
+            userInput2 = input("Enter another number")
+            if userInput == 'q' or 'Q':
+                print(SUM)
+
+def problem2():
+    userInput = input("Give me two numbers")
+def do_the_math(input):
+    SUM = num1 + num2
+    DIFFERENCE = num1 + num2
+    PRODUCT = num1 * num2
+    QUOTIENT = num1/num2
+dict = {SUM, DIFFERENCE, PRODUCT, QUOTIENT}
+return dict(input)
+print("input")
+
+
+
